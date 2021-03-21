77b13d1531078bd451643f25a061bdfd94ed6ea6
Lyndon <lyndonrenaud98@protonmail.com>
2021-03-21 13:30:34 -0400

    implements uniform value generator

--- generate.py
+++ generate.py
@@ -1,6 +1,16 @@
 def genUniformValues():
     a = 16807
     m = 2**31 - 1
+    e = 43
+    x_prev = 61
+    values = [x_prev]
+
+    for i in range(1, 100):
+        x = (a*x_prev + e) % m
+        values.append(x / m)
+        x_prev = x
+
+    return values
 
 
 def genExponentialValues():
